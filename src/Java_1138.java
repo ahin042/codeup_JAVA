27728c31e2bc64f65bd93a35e840ebf9e5ad366b
ahin0422 <ahinahn0422@gmail.com>
2026-07-31 01:45:38 +0900

    add

---
+++ src/Java_1138.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java_1138 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a == 0) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
---
+++ src/Java_1139.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Java_1139 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a == b && a == 1) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
